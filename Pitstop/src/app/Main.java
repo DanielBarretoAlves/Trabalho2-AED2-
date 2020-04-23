f1324de55e52dc55b16dbf45a5251ba0fdaa1831
Daniel Barreto <danielbarreto559@gmail.com>
2020-04-23 12:17:14 -0300

    Adiciona X pessoas a uma fila

--- Pitstop/src/app/Main.java
+++ Pitstop/src/app/Main.java
@@ -1,12 +1,29 @@
 package app;
 
+import java.util.Scanner;
+
 public class Main {
+    static void enjoyQueue(int size, Queue<Person> fila) {
+        Scanner scan = new Scanner(System.in);
+        for (int i = 0; i < size; i++) {
+            System.out.println("Informe o nome da pessoa:");
+            String nome = scan.nextLine();
+            System.out.println("Informe seu telefone (SOMENTE NUMEROS)");
+            int num = scan.nextInt();
+            Person person = new Person(nome, num);
+            fila.putOn(person);
+        }
+    }
+
     public static void main(String[] args) throws Exception {
         System.out.println("Hello Java");
+        Scanner scan = new Scanner(System.in);
         // Queue<Integer> fila = new Queue()<>(3);
         Queue<Person> fila = new Queue<>(3);
-        Person daniel = new Person("Daniel");
-        fila.putOn(daniel);
+        System.out.println("Informe a quantidade de pessoas");
+        int size = scan.nextInt();
+        enjoyQueue(size, fila);
+
         // fila.putOn(1);
         // fila.putOn(2);
         // fila.putOn(3);
@@ -15,6 +32,3 @@ public class Main {
         System.out.println(fila);
     }
 }
-
-
-
