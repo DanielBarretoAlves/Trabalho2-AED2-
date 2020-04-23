9d4fcad747329d28f9e55f5429f8eb5500be36e4
Daniel Barreto <danielbarreto559@gmail.com>
2020-04-23 13:00:46 -0300

    Comentarios

--- Pitstop/src/app/Stack.java
+++ Pitstop/src/app/Stack.java
@@ -11,53 +11,62 @@ public class Stack {
         this.qtdNode = 0;
     }
 
-    public void stackUp(Ticket t){
-        if(isEmpty() == true){
-           Node newNode = new Node(t);
-           this.top = newNode;
-           this.bot = newNode;
-           newNode.setNext(this.top);
-           qtdNode++;
-           System.out.println(newNode.toString());
-       }else{
-           Node newNode = new Node(t);
-           this.top.setNext(newNode);
-           this.top = newNode;  
-           qtdNode++;
-           System.out.println(newNode.toString());
-               
-           }
-           
-       }
+    // public void inserirUltimo(Pessoa p) {
+    //     No novoNO = new No(p);
+    //     if (this.eVazia()) {
+    //         this.prim = novoNO;
+    //     } else {
+    //         this.ult.setProx(novoNO);
+    //     }
+    //     this.ult = novoNO;
+    //     this.qtdNo++;
+    // }// ----------------------------------------------------INSERIR ULTIMO
 
-       public boolean isEmpty(){return (bot == null);}
+    public void stackUp(Ticket t) {
+        if (isEmpty() == true) {
+            Node newNode = new Node(t);
+            this.top = newNode;
+            this.bot = newNode;
+            newNode.setNext(this.top);
+            qtdNode++;
+            System.out.println(newNode.toString());
+        } else {
+            Node newNode = new Node(t);
+            this.top.setNext(newNode);
+            this.top = newNode;
+            qtdNode++;
+            System.out.println(newNode.toString());
 
-       public Node getTop() {
-          return top;   
-       }
+        }
 
-       public void setTop(Node top) {
-           this.top = top;
-       }
+    }
 
-       public Node getBot() {
-           return bot;
-       }
+    public boolean isEmpty() {
+        return (bot == null);
+    }
 
-       public void setBot(Node bot) {
-           this.bot = bot;
-       }
+    public Node getTop() {
+        return top;
+    }
 
-       public int getQtdNode() {
-           return qtdNode;
-       }
+    public void setTop(Node top) {
+        this.top = top;
+    }
 
-       public void setQtdNode(int qtdNode) {
-           this.qtdNode = qtdNode;
-       }
+    public Node getBot() {
+        return bot;
+    }
 
-    
-    
+    public void setBot(Node bot) {
+        this.bot = bot;
+    }
 
+    public int getQtdNode() {
+        return qtdNode;
+    }
+
+    public void setQtdNode(int qtdNode) {
+        this.qtdNode = qtdNode;
+    }
 
 }
