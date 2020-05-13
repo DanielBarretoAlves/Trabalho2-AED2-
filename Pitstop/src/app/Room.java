0a2c619fc60d59cbe71463930fe305e12f724036
Daniel Barreto <danielbarreto559@gmail.com>
2020-05-13 11:14:10 -0300

    Sorteano Randomicamente as Cenas dos FIlmes

--- Pitstop/src/app/Room.java
+++ Pitstop/src/app/Room.java
@@ -1,6 +1,7 @@
 package app;
 
 import java.io.*;
+import java.util.Random;
 
 public class Room {
 
@@ -12,18 +13,30 @@ public class Room {
 
     // TODO: add text reader method inside this class constructor
     public Room(int quantity, int roomNumber) throws IOException {
-        //TODO: Setar a quantidade de linhas
-        File movies  = new File("Pitstop/src/movies.txt");
-        BufferedReader br  = new BufferedReader(new FileReader(movies));
+        Random r = new Random();
+        File movies = new File("Pitstop/src/movies.txt");
+        BufferedReader br = new BufferedReader(new FileReader(movies));
         String st;
         int scenesNumber = 0;
-        //TODO: Sort the postions for atribuition
-        while ((st= br.readLine()) != null && scenesNumber < this.scenes.length) {
-            // System.out.println(st);
-            scenes[scenesNumber] = st;
+        // TODO: Sort the postions for atribuition A FORMA MAIS BURRA DE TODAS MAS VAI
+        // FUNCIONAR DEPOIS ARRUMO
+        //TODO: 2 Whiles e um for pra fazer isso, da pra melhorar mas ainda n sei como
+        int numFilmes = 0;
+        String lt;
+        while ((lt = br.readLine()) != null) {
+            numFilmes++;
+        }
+        String[] allMovies = new String[numFilmes];
+        while ((st = br.readLine()) != null) {
+            allMovies[scenesNumber] = st;
             scenesNumber++;
         }
+        //Esse for add 5 filmes aleatorios dentro de scenes
+        for (int i = 0; i < scenes.length; i++) {
 
+            scenes[i] = allMovies[r.nextInt(numFilmes)];
+        }
+        // -----------------------------------------------------------------------------------------------------
 
         this.quantity = quantity;
         this.roomNumber = roomNumber;
