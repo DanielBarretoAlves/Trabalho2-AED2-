1415fbc7d1f38b51ba700c854744a2b1ff2ff32d
DanielBarretoAlves <danielbarreto559@gmail.com>
2020-05-20 15:36:18 -0300

    Restarting

--- Pitstop/src/app/Main.java
+++ Pitstop/src/app/Main.java
@@ -1,33 +1,7 @@
 package app;
 
-import java.util.Scanner;
-
 public class Main {
-    static void enjoyQueue(int size, Queue<Person> fila) {
-        Scanner scan = new Scanner(System.in);
-        for (int i = 0; i < size; i++) {
-            System.out.println("Informe o nome da pessoa:");
-            String nome = scan.next();
-            System.out.println("Informe seu telefone (SOMENTE NUMEROS)");
-            int num = scan.nextInt();
-            Person person = new Person(nome, num);
-            fila.putOn(person);
-        }
-    }
-
-    public static void main(String[] args) throws Exception {
-        // System.out.println("Hello Java");
-        Log.flow();
-        // Scanner scan = new Scanner(System.in);
-        // // Queue q = new Queue<>();
-        // // enjoyQueue(3, q);
-        // // System.out.println(q);
-        // // System.out.println("------------------------------------------");
-        // // q.remove();
-        // // System.out.println(q);
-        // // System.out.println("------------------------------------------");
-        // // System.out.println("Primeiro Da Fila: "+q.getFirst());
-        // Room r = new Room(15, 1);
-        // System.out.println("END");
+    public static void main(String[] args){
+        System.out.println("Hello World");
     }
 }
--- Pitstop/src/app/Room.java
+++ Pitstop/src/app/Room.java
@@ -1,53 +1,37 @@
 package app;
 
-import java.io.*;
-import java.util.Random;
-
 public class Room {
-
-    private int quantity;
+    private int space;
+    private int people;
+    private String movies;
     private int roomNumber;
-    // TODO: a string movie tem que ser um array de 5 posições e o set dela vai ter
-    // que ser diferenciado
-    private String[] scenes = new String[5];
 
-    // TODO: add text reader method inside this class constructor
-    public Room(int quantity, int roomNumber) throws IOException {
-        Random r = new Random();
-        File movies = new File("Pitstop/src/movies.txt");
-        BufferedReader br = new BufferedReader(new FileReader(movies));
-        String st;
-        int scenesNumber = 0;
-        
-        // FUNCIONAR DEPOIS ARRUMO
-        //TODO: 2 Whiles e um for pra fazer isso, da pra melhorar mas ainda n sei como
-        int numFilmes = 0;
-        String lt;
-        while ((lt = br.readLine()) != null) {
-            numFilmes++;
-        }
-        String[] allMovies = new String[numFilmes];
-        while ((st = br.readLine()) != null) {
-            allMovies[scenesNumber] = st;
-            scenesNumber++;
-        }
-        //Esse for add 5 filmes aleatorios dentro de scenes
-        for (int i = 0; i < scenes.length; i++) {
-
-            scenes[i] = allMovies[r.nextInt(numFilmes)];
-        }
-        // -----------------------------------------------------------------------------------------------------
-
-        this.quantity = quantity;
-        this.roomNumber = roomNumber;
+    public Room()
+    {
+        setMovies();
+        this.people=0;
+        this.space=20;
     }
 
-    public int getQuantity() {
-        return quantity;
+    public int getSpace() {
+        return space;
     }
 
-    public void setQuantity(int quantity) {
-        this.quantity = quantity;
+    public int getPeople() {
+        return people;
+    }
+
+    public void setPeople(int people) {
+        this.people = people;
+    }
+
+    public String getMovies() {
+        return movies;
+    }
+
+    //TODO: make a real setMovies
+    public void setMovies() {
+        this.movies = "movieA, movieB, movieC, movieD";
     }
 
     public int getRoomNumber() {
@@ -57,11 +41,5 @@ public class Room {
     public void setRoomNumber(int roomNumber) {
         this.roomNumber = roomNumber;
     }
-
-    // TODO: Simplify toString for better information sharing
-    @Override
-    public String toString() {
-        return "Room [quantity=" + quantity + ", roomNumber=" + roomNumber + "]";
-    }
-
+    
 }
