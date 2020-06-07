422bfb21f87db98e14cfa0ecf9e6ff9ff650a4ea
DanielBarretoAlves <danielbarreto559@gmail.com>
2020-06-07 10:54:12 -0300

    Queue Manual Updated

--- Pitstop/src/app/Main.java
+++ Pitstop/src/app/Main.java
@@ -1,103 +1,33 @@
 package app;
 
-import java.util.Scanner;
-
-import javax.management.Query;
-
 public class Main {
-    // static void enjoyQueue(int size, Queue<Person> fila) {
-    //     Scanner scan = new Scanner(System.in);
-    //     for (int i = 0; i < size; i++) {
-    //         System.out.println("Informe o nome da pessoa:");
-    //         String nome = scan.next();
-    //         System.out.println("Informe seu telefone (SOMENTE NUMEROS)");
-    //         int num = scan.nextInt();
-    //         Person person = new Person(nome, num);
-    //         fila.putOn(person);
-    //     }
-    // }
 
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello Java");
-        Scanner scan = new Scanner(System.in);
-        Stack st = new Stack();
-        Room r1 = new Room(20, 1, "abc do amor");
-        Room r2 = new Room(20, 2, "abc do amor");
-        // Person p = new Person<>("Daniel");
-        // Room r = new Room(20, 5, "sou foda");
-        // Ticket ta = new Ticket(r);
-        // ta.setuData(p);
-        // st.stackUP(ta);
+    public static void main(String[] args) {
 
-        for (int i = 1; i < 20; i++) {
-            Ticket t = new Ticket(r1);
-            st.stackUP(t);
-            r1.setQuantity(r1.getQuantity() + 1);
-        }
-        for (int i = 1; i < 20; i++) {
-            Ticket t = new Ticket(r2);
-            st.stackUP(t);
-            r2.setQuantity(r2.getQuantity() + 1);
-        }
-        System.out.println(st.getLast().getTicket().getRoom().getRoomNumber());
-        System.out.println("=====================================================================================");
-
-        Person p1 = new Person("Daniel");
-        Person p2 = new Person("Bread");
-        Person p3 = new Person("Graci");
-        Person p4 = new Person("Celly");
-        Person p5 = new Person("Lo");
-
-        Queue q = new Queue(5);
-        q.addElement(p1);
+        Queue q = new Queue();
+        Person p = new Person("Daniel");
+        Person p2 = new Person("John");
+        q.addElement(p);
         q.addElement(p2);
-        q.addElement(p3);
-        q.addElement(p4);
-        q.show();
-        System.out.println("Ingresso para o Primeiro: " + q.getFirst().getNome());
-        st.getLast().getTicket().setuData(q.getFirst());
+        System.out.println(q);
+        System.out.println("Removing.. " + q.getFirst().getNome());
         q.remove();
-        st.showStack();
-        System.out.println("---------------------------------------------------");
-        q.show();
-        st.getLast().getTicket().setuData(q.getFirst());
-        q.remove();
-        st.showStack();
+        System.out.println(q);
 
     }
 }
 
-// TODO: ADD AS ROOMS NO TICKET - UM LOOP I DE UM A 10 N POSIÇÃO 1 DE I TEM UM
-// LOOP J DE UM A 10 ONDE TODAS AS ROOMS NESSE J TEM O ENDEREÇO COM O VALOR DE I
-
-// TODO: O GET NOME ESTÁ NA CLASSE LIST DEVE SER ADAPATADO PARA RECEBER O NUMERO
-// DA SALA ESTÁ COMENTADO WARNING!
-
-// OBS: CADA ROOM VAI PEDIR FILME[Uma String com 5 Filmes +=] E NUMERO DE
-// PESSOAS PERMITIDOS, OS FILMES VOU
-// FAZER UMA METODO QUE SETTA TODOS EM UM ARRAY DAI É SÓ DA SORT NELES QUE
-// ENTREGA NORMAL
-
 // =================================================================================================
 // MANUAL DE INSTRUÇÃO DA QUEUE F:
-// F PRINTAR FILA.
+// OBS: A FILA NO CONTRUTOR DEVE TER UM TAMANHO ESPCEIFICO (capacity)
+// SELECIONADO CASO..
+// ..CONTRARIO O TAMANHO PADÃO SERÁ 10
+
+// F GETSIZE() RETONA O TAMANHO DA FILA EM INT
 // F.GETFIRST() RETORNA O PRIMEIRO ELEMENTO DA FILA.
 // F.REMOVE() REMOVE UM ELEMENTO DA FILA.
-// F.PUTON(PERSON) ADD UM OBJETO DO TIPO PERSON NA FILA.(REQUER UMA PERSON
-// CRIADA PARA ADD NA FILA).
+// F.ADDELEMENT(PERSON) ADD UM OBJETO DO TIPO PERSON NA FILA.(REQUER UMA
+// PERSON..
+// ..CRIADA PARA ADD NA FILA).
+// METODO TO STRING CRIADO ELE MOSTRA O NOME E A POSIÇÃO DE TODOS DA FILA
 // =================================================================================================
-
-// =================================================================================================
-// MANUAL DE INSTRUÇÃO DA STACK Q:
-// Q = PRINTAR A STACK (CHAMA A TOSTRING).
-// Q.STACKTP(TICKET) = ADD UM TICKET NA CLASSE STACK OBSERVE QUE INICIALMENTE
-// TICKET NÃO REQUER PARAMETROS.
-// Q.UNSTACK() REMOVE UM ITEM DA STACK (não vai dar problema se eu remover
-// arquivos de uma stack vazia).
-// Q.getLast() Retorna o Ultimo Nó na Stack.
-// =================================================================================================
-
-// TODO: VOU TER QUE PEGAR MINHA FILA E PEGAR MINHA PILHA PRECISO DE UM METODO
-// QUE RETORNA O PRIMEIRO DA FILA E UM METODO QUE RETORNA O ULTIMO DA PILHA
-// FAZER AS INTERAÇÕES NECESSARIAS ENTRE ELES, ARAMAZENAR O NECESSARIO E DEPOIS
-// DESPACHALOS
--- Pitstop/src/app/Queue.java
+++ Pitstop/src/app/Queue.java
@@ -10,6 +10,7 @@ public class Queue {
         this.size = 0;
         this.capacity = capacity;
     }
+
     public Queue() {
         this.pessoa = new Person[10];
         this.size = 0;
@@ -41,42 +42,47 @@ public class Queue {
         return true;
     }
 
-    public void show() {
-        for (int i = 0; i < this.size; i++) {
-            System.out.println("Pos: " + (i + 1) + " - " + this.pessoa[i].getNome());
-        }
-    }
-
-
-    public Boolean remove(){
+    public Boolean remove() {
 
         if (isEmpity()) {
             return false;
         }
 
         for (int i = 0; i < size; i++) {
-            this.pessoa[i] = this.pessoa[i+1];
+            this.pessoa[i] = this.pessoa[i + 1];
         }
         this.size--;
         return true;
     }
 
-    public Person getFirst()
-    {
+    public Person getFirst() {
         if (isEmpity()) {
             return null;
         }
         return this.pessoa[0];
     }
+
+    @Override
+    public String toString() {
+        String str = "";
+        for (int i = 0; i < this.size; i++) {
+            str += ("Pos: " + (i + 1) + " - " + this.pessoa[i].getNome()) + "\n";
+        }
+        return str;
+    }
 }
 
 // =================================================================================================
 // MANUAL DE INSTRUÇÃO DA QUEUE F:
-// OBS: A FILA NO CONTRUTOR DEVE TER UM TAMANHO ESPCEIFICO (capacity) SELECIONADO CASO..
+// OBS: A FILA NO CONTRUTOR DEVE TER UM TAMANHO ESPCEIFICO (capacity)
+// SELECIONADO CASO..
 // ..CONTRARIO O TAMANHO PADÃO SERÁ 10
+
 // F GETSIZE() RETONA O TAMANHO DA FILA EM INT
 // F.GETFIRST() RETORNA O PRIMEIRO ELEMENTO DA FILA.
 // F.REMOVE() REMOVE UM ELEMENTO DA FILA.
-// F.PUTON(PERSON) ADD UM OBJETO DO TIPO PERSON NA FILA.(REQUER UMA PERSON
-// CRIADA PARA ADD NA FILA).
+// F.ADDELEMENT(PERSON) ADD UM OBJETO DO TIPO PERSON NA FILA.(REQUER UMA
+// PERSON..
+// ..CRIADA PARA ADD NA FILA).
+// METODO TO STRING CRIADO ELE MOSTRA O NOME E A POSIÇÃO DE TODOS DA FILA
 // =================================================================================================
