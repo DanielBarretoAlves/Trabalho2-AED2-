fb5a5f6a14b1726298723107fdeb034397ea5d80
DanielBarretoAlves <danielbarreto559@gmail.com>
2020-06-19 11:08:44 -0300

    Log finalizadao

--- Pitstop/src/app/Main.java
+++ Pitstop/src/app/Main.java
@@ -1,9 +1,11 @@
 package app;
 
+import java.io.File; // Import the File class
+import java.io.FileWriter;
+import java.io.IOException; // Import the IOException class to handle errors
 import java.util.Scanner;
 
 public class Main {
-    
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
@@ -19,13 +21,13 @@ public class Main {
         Log.content += " - Criou um ARRAY de Rooms com " + rooms.length + " espaços ! \n";
         for (int i = 0; i < rooms.length; i++) {
             Room r = new Room(0, i, ("Combos de Filmes: " + i));
-            Log.content+=" - Add um Room no ARRAY rooms \n";
+            Log.content += " - Add um Room no ARRAY rooms \n";
             rooms[i] = r;
         }
         // CRIAR UMA PILHA DE INGREÇOS
         Stack empiTickets = new Stack();
-        Log.content+="---------------------------------------- \n";
-        Log.content+="Adcionando TICKETS ao stack... \n";
+        Log.content += "---------------------------------------- \n";
+        Log.content += "Adcionando TICKETS ao stack... \n";
 
         for (int i = 0; i < rooms.length; i++) {
             while (rooms[i].getQuantity() < 5) {
@@ -34,26 +36,26 @@ public class Main {
                 rooms[i].setQuantity(rooms[i].getQuantity() + 1);
             }
         }
-        Log.content+="Tickets Add Com Sucesso! \n";
-        Log.content+="*********************************\n";
+        Log.content += "Tickets Add Com Sucesso! \n";
+        Log.content += "*********************************\n";
 
-        //TODO: Need Checkup
-        Log.content+=empiTickets + " \n";
-        Log.content+="*********************************\n";
+        // TODO: Need Checkup
+        Log.content += empiTickets + " \n";
+        Log.content += "*********************************\n";
 
         // CRIAR UMA FILA DE PESSOAS
         Queue people = new Queue(15);
-        Log.content+="------------------------------------- \n";
-        Log.content+="Adcionando Pessoas a QUEUE \n";
+        Log.content += "------------------------------------- \n";
+        Log.content += "Adcionando Pessoas a QUEUE \n";
         for (int i = 0; i < 10; i++) {
-            Person p = new Person("pessoa "+i, "(27)111-111");
-            Log.content+=" - Pessoa: " + p.getNome() + " Adicionada a QUEUE\n";
+            Person p = new Person("pessoa " + i, "(27)111-111");
+            Log.content += " - Pessoa: " + p.getNome() + " Adicionada a QUEUE\n";
             people.push(p);
         }
-        Log.content+="Pessoas Adcionadas com Sucesso!  \n";
-        Log.content+=people;
-        
-        //Criar um stack com os dados dos cliente armazenados
+        Log.content += "Pessoas Adcionadas com Sucesso!  \n";
+        Log.content += people;
+
+        // Criar um stack com os dados dos cliente armazenados
         Stack clients = new Stack();
         int qtt = people.getSize();
         for (int i = 0; i < qtt; i++) {
@@ -63,17 +65,26 @@ public class Main {
             people.pop();
             clients.push(t);
         }
-        Log.content +="\n############################################# \n";
-        Log.content +="Dados dos Usuários que pegaram o ingresso \n";
-        Log.content +="############################################# \n";
+        Log.content += "\n############################################# \n";
+        Log.content += "Dados dos Usuários que pegaram o ingresso \n";
+        Log.content += "############################################# \n";
         Log.content += clients + "\n";
-        Log.content +="########################### \n";
+        Log.content += "########################### \n";
 
         Log.content += "------------------------------------------- \n";
         Log.content += "           Fim da Execução \n";
         Log.content += "------------------------------------------- \n";
-        
-        System.out.println(Log.content);
+
+        try {
+            FileWriter myWriter = new FileWriter("log.txt");
+            myWriter.write(Log.content);
+            myWriter.close();
+            System.out.println("Log Criado com Sucesso!");
+        } catch (IOException e) {
+            System.out.println("Deu Erro na hora de criar o Log");
+            e.printStackTrace();
+        }
+        // System.out.println(Log.content);
     }
 }
 
